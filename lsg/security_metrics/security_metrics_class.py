40bfa1fc0e7807524d29252a78792fea656a78af
Julien Petot <48205834+jpetot@users.noreply.github.com>
2022-01-10 16:16:50 +0100

    fix(metrics): remove unused code and projection code

--- lsg/security_metrics/security_metrics_class.py
+++ lsg/security_metrics/security_metrics_class.py
@@ -38,34 +38,6 @@ class Security_metrics:
             self._distances,
         )
 
-    def correlation_protection_rate(self, variable_list):
-        """Apply the correlation protection rate metric.
-
-        Arguments:
-            variable_list {list} -- name of the variables known by the attacker
-        """
-        are_first_hit = avatars_are_first_hit(
-            self._coord_original, self._coord_avatar, distance_metric="minkowski"
-        )
-        protection_rate = hidden_rate(are_first_hit)
-
-        return correlation_protection_rate(
-            self._records_set,
-            self._avatars_set,
-            variable_list,
-            worst_case_protection_rate=protection_rate,
-        )
-
-    def inference_metrics(self, variable_list, target=None):
-        """Apply the attack by inference metric.
-
-        Arguments:
-            variable_list {list} -- name of the variables known by the attacker
-        """
-        return inference_metrics(
-            self._records_set, self._avatars_set, variable_list, target
-        )
-
     def fit_transform(self, records_set, avatars_set, variable_list):
         """Apply all the metrics and builds a report dataframe.
 
@@ -78,9 +50,12 @@ class Security_metrics:
             dataframe -- report of the metrics
         """
         # Projection
-        pr = Projection()
-        coord_original, _ = pr.fit_transform(records_set)
-        coord_avatar = pr.transform(avatars_set)
+        _, model, param = saiph.fit(records_set, nf=nf)
+        self._coord_avatar = saiph.transform(avatars_set, model, param)
+        self._coord_original = saiph.transform(records_set, model, param)
+
+        coord_original = self._coord_original
+        coord_avatar = self._coord_avatar 
 
         # Preparation
         are_first_hit = avatars_are_first_hit(
@@ -94,33 +69,16 @@ class Security_metrics:
             coord_original, coord_avatar, distances, subset_indices=None
         )
 
-        corr_protection = correlation_protection_rate(
-            records_set,
-            avatars_set,
-            variable_list,
-            worst_case_protection_rate=protection_rate,
-        )
-
-        # FIXME: target is commented because it is undefined
-        inference_attack = inference_metrics(
-            records_set,
-            avatars_set,
-            variable_list,  # target
-        )
-
         d = {
             "rate": [
                 protection_rate,
-                corr_protection,
                 (1 - local_cloak["empty_rate"]) * 100,
-                inference_attack["inference_hidden_rate"] * 100,
             ],
             "median": [
                 None,
                 None,
-                (local_cloak["avatars_median"] + local_cloak["records_median"]) / 2,
-                inference_attack["inference_local_cloaking"],
-            ],
+                (local_cloak["avatars_median"] + local_cloak["records_median"]) / 2,FloatingPointErro
+            ]
         }
 
         data_frame = pd.DataFrame(
