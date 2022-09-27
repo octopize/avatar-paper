f3e336bec56afdec78bb5cb308a13fd49ed15a06
Julien Petot <48205834+jpetot@users.noreply.github.com>
2022-09-27 10:03:43 +0200

    feat(metrics): update dcr and nndr

--- metrics/privacy_metrics/dcr_nndr.py
+++ metrics/privacy_metrics/dcr_nndr.py
@@ -8,7 +8,10 @@ from metrics.privacy_metrics.distance import get_distances_closest_records
 def get_dcr(
     original_coordinates: pd.DataFrame, avatar_coordinates: pd.DataFrame
 ) -> List[float]:
-    """Get distances to the closest records."""
+    """Get distances to the closest records.
+
+    DCR is the distance of each synthetic record to a record in the original dataset.
+    """
     indices_distances = get_distances_closest_records(
         original_coordinates, avatar_coordinates, searching_frame=1
     )
@@ -19,7 +22,11 @@ def get_dcr(
 def get_nndr(
     original_coordinates: pd.DataFrame, avatar_coordinates: pd.DataFrame
 ) -> List[float]:
-    """Get nearest neighbors distance ratio."""
+    """Get nearest neighbors distance ratio.
+
+    Ratio of the distance of each synthetic record to its closest
+    to the second closest record in the original dataset.
+    """
     indices_distances = get_distances_closest_records(
         original_coordinates, avatar_coordinates, searching_frame=2
     )
--- metrics/privacy_metrics/distance.py
+++ metrics/privacy_metrics/distance.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
-from numpy.typing import NDArray
+from numpy.typing import NDArray, List, Tuple
 
 from metrics.faiss_knn import FaissKNeighbors
-
 
 def get_distances_closest_records(
     records: pd.DataFrame, synthetic: pd.DataFrame, searching_frame: int
@@ -14,7 +13,7 @@ def get_distances_closest_records(
     ---------
         records: Original records
         synthetic: Synthetic data
-        searching_frame: number of neighbor to found
+        searching_frame: number of neighbors to find
     Returns
     -------
         indices_distances: indices, distances nearest neighbor among original records.
@@ -23,9 +22,9 @@ def get_distances_closest_records(
     nn = FaissKNeighbors(k=searching_frame)
     nn.fit(np.array(records))
 
-    distances, indices = nn.index.search(
-        synthetic.to_numpy().astype(np.float32), k=searching_frame
-    )
+    # index.search returns two arrays (distances, indices)
+    # https://github.com/facebookresearch/faiss/wiki/Getting-started
+    distances, indices = nn.predict(synthetic.to_numpy().astype(np.float32))
 
     indices_distances = list(zip(indices, distances))
     return indices_distances
